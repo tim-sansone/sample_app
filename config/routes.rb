c1e20c11339c6afcced752ac4041138fb3e5ab54
Tim Sansone <tim.sansone@gmail.com>
2022-11-21 21:04:12 +0000

    updated root route and test suite

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#hello"
+
 end
